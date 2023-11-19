911ad32ffa3e34b6d0c7de10671ca50376651d7f
daveg7lee <daveg7lee@gmail.com>
2023-11-19 19:07:31 +0900

    url updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_URL || "https://equipage.vercel.app",
+  siteUrl: process.env.NEXT_PUBLIC_URL || "https://www.equipage.dev/",
   generateRobotsTxt: true,
 };
